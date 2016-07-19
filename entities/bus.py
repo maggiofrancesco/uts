e30a541fb8ac662f420f911d51d2ec5c68f9bf3c
Francesco Maggio <francescomaggio11@gmail.com>
2016-07-19 11:36:00 +0200

    Refactoring entities bus and request

--- entities/bus.py
+++ entities/bus.py
@@ -3,8 +3,8 @@
 
 class Bus(object):
 
-    def __init__(self, id, license_plate, seats, lat, lon, place="Address not available"):
-        self.id = id
+    def __init__(self, id_bus, license_plate, seats, lat, lon, place="Address not available"):
+        self.id_bus = id_bus
         self.license_plate = license_plate
         self.seats = seats
         self.lat = lat
@@ -13,5 +13,5 @@ class Bus(object):
 
     def __str__(self):
         return "ID Mezzo: {0} - Targa: {1} - Posti a sedere: {2} - Lat: {3} - Lon: {4} - Indirizzo luogo: {5}".format(
-            self.id, self.license_plate, self.seats, self.lat, self.lon, self.place
+            self.id_bus, self.license_plate, self.seats, self.lat, self.lon, self.place
         )
--- entities/request.py
+++ entities/request.py
@@ -3,9 +3,9 @@
 
 class Request(object):
 
-    def __init__(self, id, lat_dep, lon_dep, lat_arr, lon_arr, departure="Address not available",
-                 arrival="Address not available", time_dep=None, time_arr=None, range_arrival=None ):
-        self.id = id
+    def __init__(self, id_request, lat_dep, lon_dep, lat_arr, lon_arr, departure="Address not available",
+                 arrival="Address not available", time_dep=None, time_arr=None, range_arrival=None):
+        self.id_request = id_request
         self.lat_dep = lat_dep
         self.lon_dep = lon_dep
         self.lat_arr = lat_arr
@@ -19,6 +19,6 @@ class Request(object):
     def __str__(self):
         return "ID Richiesta: {0} - Lat. Partenza: {1} - Lon. Partenza: {2} - Lat. Arrivo: {3} - Lon. Arrivo: {4}" \
                " - Indirizzo partenza: {5} - Indirizzo arrivo: {6} - Time partenza: {7} - Time arrivo: {8} - Range arrivo: {9}".format(
-            self.id, self.lat_dep, self.lon_dep, self.lat_arr, self.lon_arr,
+            self.id_request, self.lat_dep, self.lon_dep, self.lat_arr, self.lon_arr,
             self.departure, self.arrival, self.time_dep, self.time_arr, self.range_arrival
         )
