49fab62858639408500bc6351bf92a0a1e7239bf
Francesco Maggio <francescomaggio11@gmail.com>
2016-07-22 16:03:09 +0200

    Refactoring in request entity

--- entities/request.py
+++ entities/request.py
@@ -3,8 +3,8 @@
 
 class Request(object):
 
-    def __init__(self, id_request, lat_dep, lon_dep, lat_arr, lon_arr, departure="Address not available",
-                 arrival="Address not available", time_dep=None, time_arr=None, range_arrival=None):
+    def __init__(self, id_request, lat_dep, lon_dep, lat_arr, lon_arr, departure='',
+                 arrival='', time_dep=None, time_arr=None, user=None):
         self.id_request = id_request
         self.lat_dep = lat_dep
         self.lon_dep = lon_dep
@@ -14,11 +14,11 @@ class Request(object):
         self.arrival = arrival
         self.time_dep = time_dep
         self.time_arr = time_arr
-        self.range_arrival = range_arrival
+        self.user = user
 
     def __str__(self):
         return "ID Richiesta: {0} - Lat. Partenza: {1} - Lon. Partenza: {2} - Lat. Arrivo: {3} - Lon. Arrivo: {4}" \
                " - Indirizzo partenza: {5} - Indirizzo arrivo: {6} - Time partenza: {7} - Time arrivo: {8} - Range arrivo: {9}".format(
             self.id_request, self.lat_dep, self.lon_dep, self.lat_arr, self.lon_arr,
-            self.departure, self.arrival, self.time_dep, self.time_arr, self.range_arrival
+            self.departure, self.arrival, self.time_dep, self.time_arr, self.user
         )
