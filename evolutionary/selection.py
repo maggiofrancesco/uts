208ed6ad235ff20a245ec453bf29cf6514068d52
Francesco Maggio <francescomaggio11@gmail.com>
2016-07-31 19:55:20 +0200

    Implemented methods for selection between two generations

--- evolutionary/selection.py
+++ evolutionary/selection.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
 
+"""
+Seleziona le matrici con il valore di fitness più basso all'interno di una specifica generazione.
+"""
+def min_fitness(population):
 
-def min_fitness():
-    pass
+    exchanges = len(population) / 4
+    exchanges = 1 if exchanges == 0 else exchanges
+    ordered_by_distance = sorted(population, key=lambda x: x.fitness_data["distance"])
+    return ordered_by_distance[:exchanges]
 
+"""
+Seleziona le matrici con il valore di fitness più elevato all'interno di una specifica generazione.
+"""
+def max_fitness(population):
 
-def max_fitness():
-    pass
+    exchanges = len(population) / 4
+    exchanges = 1 if exchanges == 0 else exchanges
+    ordered_by_distance = sorted(population, key=lambda x: x.fitness_data["distance"], reverse=True)
+    return ordered_by_distance[:exchanges]
 
+"""
+Metodo per l'effuazione dello scambio di matrici tra due generazioni.
+"""
+def exchange(prev_population, next_population):
 
-def exchange():
-    pass
+    good_prev_population = min_fitness(prev_population)
+    bad_next_population = max_fitness(next_population)
+    index = 0
+    for matrix in good_prev_population:
+        next_population.remove(bad_next_population[index])
+        next_population.append(matrix)
+        index += 1
